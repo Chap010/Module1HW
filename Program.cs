ce29b1fb386dd949ddc1e85773a3da77aef8858f
Antonio <atejda8071@southwestuniversity.edu>
2021-06-01 16:30:06 -0600

    Redo / Fix module 1

--- Program.cs
+++ Program.cs
@@ -5,24 +5,41 @@ namespace Module1HW
     class Program
     {
         static void Main(string[] args)
+
+        {
+
+
+            MyMethod();
+            Console.ReadLine();
+          
+
+
+        }
+
+        public static void MyMethod()
+
         {
             string swSchool = "Cool Humble " + "Teachers";
             int computerLabs = 37;                                  // Total Computers systems
             int medicalLabs = 13;                                   // Total Medical Labs
-            int result = medicalLabs + computerLabs;                // Adding Both Totals
-
-
-            Console.WriteLine(swSchool);
-            Console.WriteLine(result);
-            Console.WriteLine(result / 2);
+            int result = medicalLabs * computerLabs;
 
             bool isItTrue = true;
-            isItTrue = medicalLabs <= computerLabs;               // Comparing Is There enough Computers
+            isItTrue = medicalLabs <= computerLabs;               // Comparing Is There enough
+                                                                  
+
             Console.WriteLine(isItTrue + " There are Enough Computers For Students");
-        }  
+            Console.WriteLine(swSchool);
+            Console.WriteLine(result + " Labs");                // multiply labs
+            Console.WriteLine(result / 2 + " Labs");            // dividing labs
+        }
+
     }
 
 }
+
+
+
 
 
 
